fd890d7cd9e41d3e6371a28ab06b0583e61ba774
b0rgib <vs.bor@yandex.ru>
2023-01-06 00:06:59 +0400

    first commit

--- menu_test/draw_menu/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DrawMenuConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'draw_menu'
--- menu_test/draw_menu/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Menu, Item
-
-def home(request):
-	return render(request, 'draw_menu/home.html', {})
-
-
